3141e144bffc00335cfc8c199e45fcd6b4d44e3f
Sergey Smirnov <ssw30@yandex.ru>
2023-07-20 10:54:03 +0400

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/MoviesManager/MoviesManagerTest.java
@@ -0,0 +1,104 @@
+package ru.netology.javaqa.MoviesManager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoviesManagerTest {
+
+    @Test
+    public void addingAListOfMovies() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        manager.add("Film 4");
+        manager.add("Film 5");
+
+
+        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addingAList6fMovies() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        manager.add("Film 4");
+        manager.add("Film 5");
+        manager.add("Film 6");
+
+
+        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5", "Film 6"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addingOneMovie() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("Film 1");
+
+        String[] expected = {"Film 1"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void dontAddAnything() {
+        MoviesManager manager = new MoviesManager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseListOf5Movies() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        manager.add("Film 4");
+        manager.add("Film 5");
+
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseListOf6Movies() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        manager.add("Film 4");
+        manager.add("Film 5");
+        manager.add("Film 6");
+
+        String[] expected = {"Film 6", "Film 5", "Film 4", "Film 3", "Film 2"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseListOf1Movies() {
+        MoviesManager manager = new MoviesManager();
+
+
+        manager.add("Film 6");
+
+        String[] expected = {"Film 6"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
